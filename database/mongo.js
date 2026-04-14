f969ba5fd8fc72ebdb54b7ed6bd8924513a9bf6b
t6260068-rgb <t6260068@gmail.com>
2026-04-14 19:28:34 +0400

    Create mongo.js

---
+++ database/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+async function connectMongo() {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is missing in environment variables");
+    }
+
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("🟢 MongoDB connected successfully!");
+  } catch (err) {
+    console.error("🔴 MongoDB connection error:", err.message);
+  }
+}
+
+module.exports = connectMongo;
