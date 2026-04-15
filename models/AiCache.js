51c4bdca4b2d75a82f20761fec65889a5c03acf8
t6260068-rgb <t6260068@gmail.com>
2026-04-15 08:49:00 +0400

    Update AiCache.js

--- models/AiCache.js
+++ models/AiCache.js
@@ -1,1 +1,33 @@
-{}
+const mongoose = require("mongoose");
+
+const rumorItemSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    gifQuery: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const aiCacheSchema = new mongoose.Schema(
+  {
+    dateKey: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    yaps: {
+      type: [String],
+      default: [],
+    },
+    rumors: {
+      type: [rumorItemSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.models.AiCache || mongoose.model("AiCache", aiCacheSchema);
