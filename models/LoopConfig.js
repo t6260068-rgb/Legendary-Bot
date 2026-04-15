cff53c703d01556d15e27e33634b05093716a045
t6260068-rgb <t6260068@gmail.com>
2026-04-15 08:48:42 +0400

    Update LoopConfig.js

--- models/LoopConfig.js
+++ models/LoopConfig.js
@@ -1,1 +1,27 @@
-{}
+const mongoose = require("mongoose");
+
+const loopConfigSchema = new mongoose.Schema(
+  {
+    guildId: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    yap: {
+      channelId: { type: String, default: null },
+      intervalMs: { type: Number, default: 1800000 },
+      active: { type: Boolean, default: false },
+    },
+    rumors: {
+      channelId: { type: String, default: null },
+      intervalMs: { type: Number, default: 3600000 },
+      active: { type: Boolean, default: false },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.models.LoopConfig || mongoose.model("LoopConfig", loopConfigSchema);
